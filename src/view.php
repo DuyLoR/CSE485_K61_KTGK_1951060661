8d9eb905e6a58fd5591387b43cfa68621eb5d140
DuyLoR <duya7k50@gmail.com>
2021-10-22 10:50:40 +0700

    create view.php, add.php, delete.php

--- src/view.php
+++ src/view.php
@@ -20,8 +20,8 @@
         <h3>Vùng quản trị</h3>
     </div>
     <div class="container">
-        <h3 class="text-center mb-4">Bảng thống kê</h3>
-        <table class="table table-dark table-hover">
+        <h3 class="text-center mb-4">Bảng thống kê chi tiết</h3>
+        <table class="table table-dark table-hover text-center">
             <thead>
                 <tr>
                     <th scope="col">Mã bài thi</th>
@@ -29,8 +29,11 @@
                     <th scope="col">Ngày thi</th>
                     <th scope="col">Thời gian làm bài</th>
                     <th scope="col">Số câu hỏi</th>
+                    <th scope="col">Điểm cho mỗi câu trả lời đúng</th>
+                    <th scope="col">Ngày tạo bài thi</th>
                     <th scope="col">Trạng thái</th>
-                    <th scope="col">Chi tiết</th>
+                    <th scope="col">Mã truy cập bài thi</th>
+                    <th scope="col">Chức năng</th>
                 </tr>
             </thead>
             <tbody>
@@ -51,14 +54,23 @@
                     <td><?php echo $row['exam_datetime']; ?></td>
                     <td><?php echo $row['duration']; ?></td>
                     <td><?php echo $row['total_question']; ?></td>
+                    <td><?php echo $row['marks_per_right_answer']; ?></td>
+                    <td><?php echo $row['created_on']; ?></td>
                     <td><?php echo $row['status']; ?></td>
-                    <td><a href="view.php">Chi tiết</a></td>
+                    <td><?php echo $row['exam_code']; ?></td>
+                    <td>
+                        <div class="row">
+                            <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn col-md-6 text-white btn-info">Sửa</a>
+                            <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn col-md-6 text-white btn-info">Xóa</a>
+                        </div>
+                    </td>
                 </tr>
                 <?php
                 }
             }
                 ?>
         </table>
+        <div class="row"><a href="add.php" class="btn col-md-2 text-white btn-info">Thêm</a></div>
     </div>
 </body>
 
